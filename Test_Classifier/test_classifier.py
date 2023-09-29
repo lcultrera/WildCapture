251ba92cb69ab2e6fa7165f68ddac80939285c82
lcultrera <128476681+lcultrera@users.noreply.github.com>
2023-09-29 12:59:05 +0200

    Update test_classifier.py

--- Test_Classifier/test_classifier.py
+++ Test_Classifier/test_classifier.py
@@ -2,20 +2,35 @@ import timm
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+from ForwardWrapper import forward_wrapper
 
 class VitModel(torch.nn.Module):
-    def __init__(self, model_name, num_classes):
+    def __init__(self, model_name, num_classes, extract_attention):
         super(VitModel, self).__init__()
         self.modelVit = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
         data_config = timm.data.resolve_model_data_config(self.modelVit)
         self.transforms = timm.data.create_transform(**data_config, is_training=True)
+        
+        if extract_attention == True:
+                    self.modelVit.blocks[-1].attn.forward = forward_wrapper(self.modelVit.blocks[-1].attn)
 
     def forward(self, x):
-        d1 = self.modelVit(x)
-        return d1
+        if extract_attention == True:
+            cls_weight = self.modelVit.blocks[-1].attn.cls_attn_map
+            cls_weight =  cls_weight.mean(dim=1)
+            a = torch.empty(cls_weight.shape[0], 1)
+            a[:,0] = cls_weight[:,165]
+            cls = torch.cat((cls_weight, a.cuda()), 1)
+            cls = cls.view(-1, 14, 14, 1).detach().cpu().numpy()
+            #cls_resized = F.interpolate(cls(1, 1, 14, 14), (224, 224), mode='bilinear').view( 224, 224, 1)
+            cls = np.squeeze(cls,axis=0)
+            return d1, cls, attn_map
+        else:
+            d1 = self.modelVit(x)
+            return d1
 
-def load_model(model_path, model_name, num_classes):
-    model = VitModel(model_name, num_classes)
+def load_model(model_path, model_name, num_classes, extract_attention):
+    model = VitModel(model_name, num_classes, extract_attention)
     model.load_state_dict(torch.load(model_path))
     model.eval()
     return model
@@ -33,19 +48,30 @@ def preprocess_image(image_path, img_size):
     return x
 
 def main():
-    model_path = 'modelTRANSFORMER_VIT' #Link to download the weights in README
-    img_size = 224
-    num_classes = 8
+    
+    # Define the path to the YAML config file
+    config_path = 'config/config.yaml'
+
+    # Load the config file
+    with open(config_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+    # Extract parameters from the config
+    num_classes = config["num_classes"] #7
+    img_size = config["img_size"] #224
+    model_path = config["model_path"] #modelTRANSFORMER_VIT
+    extract_attention = config["extract_attention"] #False
+
 
     model = load_model(model_path, 'vit_large_patch16_224.augreg_in21k_ft_in1k', num_classes)
 
-    image_path = 'crops/00000325.jpg'
+    image_path = 'crops/00000325.jpg' #open an image from crop folder
     x = preprocess_image(image_path, img_size)
 
     out, cls, attn = model(x)
     _, preds = torch.max(out, 1)
 
-    classes = ['Domestic Cattle', 'Eurasian Badger', 'European Hare', 'Grey Wolf', 'Human', 'Red Deer', 'Red Fox', 'Wild Boar']
+    classes = ['Domestic Cattle', 'Eurasian Badger', 'European Hare', 'Grey Wolf', 'Red Deer', 'Red Fox', 'Wild Boar']
     predicted_class = classes[preds.cpu().numpy()[0]]
     confidence = torch.nn.functional.softmax(out, dim=1).max().item()
 
