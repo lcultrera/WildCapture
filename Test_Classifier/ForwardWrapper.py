29aac69f2b5a3b0a071f6d4fe8748bc0ffe70c9d
lcultrera <128476681+lcultrera@users.noreply.github.com>
2023-09-29 12:50:26 +0200

    Create ForwardWrapper.py

---
+++ Test_Classifier/ForwardWrapper.py
@@ -0,0 +1,20 @@
+import torch 
+
+def forward_wrapper(attn_obj):
+    def forward_wrap(x):
+        B, N, C = x.shape
+        qkv = attn_obj.qkv(x).reshape(B, N, 3, attn_obj.num_heads, C // attn_obj.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)   # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * attn_obj.scale
+        attn = attn.softmax(dim=-1)
+        attn = attn_obj.attn_drop(attn)
+        attn_obj.attn_map = attn
+        attn_obj.cls_attn_map = attn[:, :, 0, 2:]
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = attn_obj.proj(x)
+        x = attn_obj.proj_drop(x)
+        return x
+    return forward_wrap
+
